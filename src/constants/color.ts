8f3c79bff6f7d40f1684ba620918f9be21e550ab
Imad Shah <shahimad499@gmail.com>
2025-01-19 11:41:16 +0500

    feat: add folder structure and aliases

---
+++ src/constants/color.ts
@@ -0,0 +1,15 @@
+export const COLORS = {
+  primary: {
+    main: "#ffc107",
+  },
+  secondary: {
+    main: "#fd7e14",
+  },
+  dark: {
+    main: "#000000",
+    lightDark: "#343a40",
+  },
+  white: {
+    main: "#fff",
+  },
+};
---
+++ src/styles/theme.ts
@@ -0,0 +1,58 @@
+import { createTheme } from "@mui/material";
+export let theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+  typography: {
+    h1: {
+      fontWeight: 700,
+      fontSize: "60px",
+      lineHeight: "42px",
+      marginBottom: "5px",
+    },
+    h2: {
+      fontWeight: 600,
+      fontSize: "50px",
+      lineHeight: "30px",
+      letterSpacing: "0.0015em",
+      textAlign: "left",
+    },
+    h3: {
+      fontWeight: 600,
+      fontSize: "24px",
+      lineHeight: "28px",
+      letterSpacing: "0.0015em",
+    },
+    h4: {
+      fontWeight: 500,
+      fontSize: "20px",
+      lineHeight: "21px",
+    },
+    h5: {
+      fontWeight: 500,
+      fontSize: "18px",
+      lineHeight: "29px",
+    },
+    h6: {
+      fontWeight: 500,
+      fontSize: "16px",
+      lineHeight: "23px",
+    },
+    body1: {
+      fontWeight: 500,
+      fontSize: "14px",
+      lineHeight: "20px",
+    },
+  },
+});
+
+theme = {
+  ...theme,
+  components: {},
+};
