ce078d541f46d7283dde0c7e85c65ec5ca422b7c
Imad Shah <shahimad499@gmail.com>
2025-01-19 11:42:17 +0500

    fix: fix & modifications

---
+++ src/core/App/App.tsx
@@ -0,0 +1,11 @@
+import { COLORS } from "@muc/constants";
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ backgroundColor: COLORS.primary.main }}>Hellow portfolio</h1>
+    </div>
+  );
+};
+
+export default App;
