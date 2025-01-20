98b878bb77cf42286e0a30a797554d821d031986
Imad Shah <shahimad499@gmail.com>
2025-01-20 14:43:26 +0500

    feat: add slider for home modules

--- src/modules/HomeModule/HomeModule.tsx
+++ src/modules/HomeModule/HomeModule.tsx
@@ -1,84 +1,27 @@
-import { Box, Typography } from "@mui/material";
-import { AppLayout } from "@muc/layout";
-import {
-  AnalyticsCard,
-  ContactCard,
-  CustomButton,
-  ProgressBar,
-  ProjectCards,
-  ServiceCard,
-} from "@muc/components";
-import { NearMe, Phone } from "@mui/icons-material";
+import Header from "./components/Header";
+import { Box } from "@mui/material";
+import { CustomButton } from "@muc/components";
 
 const HomeModule = () => {
   return (
-    <AppLayout>
-      <Typography>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint est
-        eligendi eius reiciendis consectetur laboriosam optio debitis
-        aspernatur, ab laudantium repellendus earum laborum sed quisquam libero
-        at facere aliquam. Laudantium.
-      </Typography>
-      <CustomButton title="My works" variant="contained" />
-      <Box sx={{ display: "flex", gap: "20px" }}>
-        <ServiceCard
-          title="Web Development"
-          icons="assets/images/service-1.svg"
-        />
-        <ServiceCard
-          title="App Development"
-          icons="assets/images/service-2.svg"
-        />
-        <ServiceCard
-          title="App Development"
-          icons="assets/images/service-3.svg"
-        />
+    <>
+      <Header />
+      <Box
+        sx={{
+          display: "flex",
+          m: "auto",
+          gap: "20px",
+          justifyContent: "center",
+          position: "absolute",
+          bottom: 340,
+          width: "100%",
+          zIndex: 999,
+        }}
+      >
+        <CustomButton title="Hire Me" variant="contained" />
+        <CustomButton title="My works" variant="outlined" />
       </Box>
-      <Box width={"50%"}>
-        <ProgressBar title="React(JS)" percentage={100} />
-        <ProgressBar title="React Native" percentage={70} />
-        <ProgressBar title="Javascript (ES6)" percentage={80} />
-        <ProgressBar title="HTML" percentage={100} />
-        <ProgressBar title="CSS" percentage={100} />
-      </Box>
-      <Box display={"flex"} gap={"30px"} flexWrap={"wrap"}>
-        <ProjectCards
-          title="Branding & landing UI"
-          category="Web Development"
-          width="410px"
-        />
-        <ProjectCards
-          title="Make Portfolio For Yourself"
-          category="Web Development"
-          width="820px"
-        />
-        <ProjectCards
-          title="Custom App for your Bussiness"
-          category="App Development"
-          width="820px"
-        />
-        <ProjectCards
-          title="App Portfolio .."
-          category="Web Development"
-          width="410px"
-        />
-      </Box>
-      <AnalyticsCard title="Awards" value={20} />
-      <AnalyticsCard title="Completed Project" value={10} />
-      <br />
-      <br />
-      <br />
-      <ContactCard
-        poster={<NearMe />}
-        title="Address"
-        details="Swat, Pakistan"
-      />
-      <ContactCard
-        poster={<Phone />}
-        title="Contact Number"
-        details="+(92) 341-2764104"
-      />
-    </AppLayout>
+    </>
   );
 };
 
---
+++ src/modules/HomeModule/components/Header.tsx
@@ -0,0 +1,40 @@
+import { Box } from "@mui/material";
+import FadeSlider from "./FadeSlider";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const Header = () => {
+  return (
+    <Box sx={{ my: -10, zIndex: -999, position: "relative", width: "100%" }}>
+      <Box
+        component={"img"}
+        src="assets/images/project.jpg"
+        sx={{
+          objectFit: "cover",
+          width: "100%",
+          height: 650,
+          filter: "brightness(20%)",
+          position: "relative",
+        }}
+      />
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          textAlign: "center",
+          m: "auto",
+        }}
+      >
+        <FadeSlider />
+      </Box>
+      
+    </Box>
+  );
+};
+
+export default Header;
