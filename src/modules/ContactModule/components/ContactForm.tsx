68c769c117cc011e6a3576174aaa1f457a697c55
Imad Shah <shahimad499@gmail.com>
2025-01-28 13:19:31 +1200

    feat: add some changes

--- src/modules/ContactModule/components/ContactForm.tsx
+++ src/modules/ContactModule/components/ContactForm.tsx
@@ -2,18 +2,45 @@ import { CustomButton, CustomTextField } from "@muc/components";
 import { COLORS } from "@muc/constants";
 import { Box, Container, Typography } from "@mui/material";
 import { FormProvider, useForm } from "react-hook-form";
+import emailjs from "emailjs-com";
 
 const ContactForm = () => {
   const methods = useForm();
   const onSendMessage = (data: any) => {
-    console.log(data);
+    emailjs
+      .send(
+        "service_6ig75c9",
+        "template_dg5i0n8",
+        {
+          fullName: data.fullName,
+          email: data.email,
+          subject: data.subject,
+          message: data.message,
+        },
+        "TNqygeT5sEUgkm8bO"
+      )
+      .then(
+        (response: any) => {
+          console.log("Message sent successfully:", response);
+        },
+        (error: any) => {
+          console.error("Failed to send message:", error);
+        }
+      );
+      methods.reset({
+        fullName: "",
+        email: "",
+        subject: "",
+        message: "",
+      });
   };
+
   return (
     <FormProvider {...methods}>
       <Container
         maxWidth="md"
         sx={{
-          p: {md:"32px",sm:'30px',xs:'15px'},
+          p: { md: "32px", sm: "30px", xs: "15px" },
           border: `1px solid ${COLORS.primary.main}`,
           borderRadius: 2,
           textAlign: "center",
@@ -29,14 +56,23 @@ const ContactForm = () => {
           variant="h2"
           fontSize={{ md: "50px", sm: "40px", xs: "20px" }}
           color={COLORS.primary.main}
-          textAlign={'center'}
+          textAlign={"center"}
         >
           Leave a message
         </Typography>
-        <Typography variant="h5" fontSize={{md:'18px',sm:'16px',xs:'15px'}} pt={3} color={COLORS.dark.lightDark}>
+        <Typography
+          variant="h5"
+          fontSize={{ md: "18px", sm: "16px", xs: "15px" }}
+          pt={3}
+          color={COLORS.dark.lightDark}
+        >
           Ready to take your business to the next level?
         </Typography>
-        <Typography variant="h5" fontSize={{md:'18px',sm:'16px',xs:'15px'}} color={COLORS.dark.lightDark}>
+        <Typography
+          variant="h5"
+          fontSize={{ md: "18px", sm: "16px", xs: "15px" }}
+          color={COLORS.dark.lightDark}
+        >
           Let’s collaborate and unlock new growth opportunities together!
         </Typography>
         <Box
@@ -73,7 +109,7 @@ const ContactForm = () => {
           </Box>
           <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
             <CustomButton
-              title="send message"
+              title="Send Message"
               variant="contained"
               width="250px"
               type="submit"
